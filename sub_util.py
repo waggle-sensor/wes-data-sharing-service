969c3323cebb661477ecf5719a1bc2c113ff60cc
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-09-29 15:57:47 -0500

    accept topics as args

--- sub_util.py
+++ sub_util.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 import random
 import mapper
@@ -7,13 +8,20 @@ def on_local_callback(ch, method, properties, body):
     print(body)
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('topics', nargs='+')
+    args = parser.parse_args()
+
     connection = pika.BlockingConnection(pika.ConnectionParameters(
         credentials=pika.PlainCredentials('worker', 'worker'),
     ))
     channel = connection.channel()
 
     queue = channel.queue_declare('', exclusive=True).method.queue
-    channel.queue_bind(queue=queue, exchange='data.topic', routing_key='env.temperature.#')
+
+    for topic in args.topics:
+        channel.queue_bind(queue=queue, exchange='data.topic', routing_key=topic)
+
     channel.basic_consume(queue, on_local_callback, auto_ack=True)
     channel.start_consuming()
 
