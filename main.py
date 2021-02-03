c777896771edcfd3818495ae399dc6679cf33517
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-02-03 15:14:19 -0600

    using new message protocol

--- main.py
+++ main.py
@@ -1,81 +1,69 @@
 import argparse
 import time
 import random
-import mapper
 import pika
 import json
 import os
 import logging
-from hashlib import sha1
+import waggle.message as message
+import re
+
 
 WAGGLE_NODE_ID = os.environ.get('WAGGLE_NODE_ID', '0000000000000000')
-
 RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST', 'rabbitmq-server')
 RABBITMQ_PORT = int(os.environ.get('RABBITMQ_PORT', '5672'))
 RABBITMQ_USERNAME = os.environ.get('RABBITMQ_USERNAME', 'service')
 RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_PASSWORD', 'service')
 
+
 def on_validator_callback(ch, method, properties, body):
-    logging.info('processing message')
-    if properties.type is None:
-        logging.warning('message missing type')
-        return
-    if properties.timestamp is None:
-        logging.warning('message missing timestamp')
-        return
-    if properties.reply_to is None:
-        logging.warning('message missing reply_to')
-        return
-    if method.routing_key in ['node', 'all']:
-        publish_to_node(ch, properties, body)
-    if method.routing_key in ['beehive', 'all']:
-        publish_to_beehive(ch, properties, body)
-    logging.info('processed message')
-
-
-def publish_to_node(ch, properties, body):
-    ch.basic_publish(
-        exchange='data.topic',
-        routing_key=properties.type,
-        properties=properties,
-        body=body)
-    logging.debug('node <- %s', body)
-
-
-def publish_to_beehive(ch, properties, body):
-    if properties.content_type is None:
-        value = body
-    elif properties.content_type == 'application/json':
-        value = json.loads(body)
-    elif properties.content_type == 'image/png':
-        value = sha1(body).hexdigest()
-        logging.info('staging image for large file transport with ref %s', value)
-
-    msg = {
-        'ts': properties.timestamp,
-        'name': properties.type,
-        'plugin': properties.reply_to,
-        'value': value,
-    }
-
+    logging.debug("processing message")
     try:
-        body = mapper.local_to_waggle(msg)
-    except Exception:
-        logging.warning('message could not be serialized %s', msg)
+        msg = message.load(body)
+    except json.JSONDecodeError:
+        logging.warning('failed to parse message %s', body)
+        ch.basic_ack(method.delivery_tag)
+        return
+    except KeyError as key:
+        logging.warning('message missing key %s', key)
+        ch.basic_ack(method.delivery_tag)
         return
 
-    # tag until we work out plan for this
-    properties.content_type = 'application/waggle'
+    # tag message with plugin and node metadata
+    
+    # rabbitmq user_id must has format "plugin.name:version"
+    match = re.match(r"plugin\.(\S+:\S+)", properties.user_id)
+    if match is None:
+        logging.warning('invalid message user ID %s', properties.user_id)
+        ch.basic_ack(method.delivery_tag)
+        return
+    msg.meta["plugin"] = match.group(1)
+    msg.meta["node"] = WAGGLE_NODE_ID
+    body = message.dump(msg)
 
-    # add self to route
-    properties.reply_to = '.'.join([WAGGLE_NODE_ID, properties.reply_to])
+    scope = method.routing_key
 
-    ch.basic_publish(
-        exchange='to-beehive',
-        routing_key=properties.type,
-        properties=properties,
-        body=body)
-    logging.debug('beehive <- %s', body)
+    if scope not in ['node', 'beehive', 'all']:
+        logging.warning('invalid message scope %s', scope)
+        ch.basic_ack(method.delivery_tag)
+        return
+
+    if scope in ['node', 'all']:
+        logging.debug('forwarding message type "%s" to local', msg.name)
+        ch.basic_publish(
+            exchange='data.topic',
+            routing_key=msg.name,
+            body=body)
+
+    if scope in ['beehive', 'all']:
+        logging.debug('forwarding message type "%s" to beehive', msg.name)
+        ch.basic_publish(
+            exchange='to-beehive',
+            routing_key=msg.name,
+            body=body)
+
+    ch.basic_ack(method.delivery_tag)
+    logging.debug('processed message')
 
 
 def declare_exchange_with_queue(ch: pika.adapters.blocking_connection.BlockingChannel, name: str):
@@ -86,7 +74,7 @@ def declare_exchange_with_queue(ch: pika.adapters.blocking_connection.BlockingCh
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose logging')
+    parser.add_argument('--debug', action='store_true', help='enable verbose logging')
     parser.add_argument('--rabbitmq-host', default=RABBITMQ_HOST, help='rabbitmq host')
     parser.add_argument('--rabbitmq-port', default=RABBITMQ_PORT, type=int, help='rabbitmq port')
     parser.add_argument('--rabbitmq-username', default=RABBITMQ_USERNAME, help='rabbitmq username')
@@ -94,7 +82,7 @@ def main():
     args = parser.parse_args()
 
     logging.basicConfig(
-        level=logging.DEBUG if args.verbose else logging.INFO,
+        level=logging.DEBUG if args.debug else logging.INFO,
         format='%(asctime)s %(message)s',
         datefmt='%Y/%m/%d %H:%M:%S')
     # pika logging is too verbose, so we turn it down.
@@ -120,9 +108,8 @@ def main():
     declare_exchange_with_queue(channel, 'to-beehive')
     
     logging.info('starting main process.')
-    channel.basic_consume('to-validator', on_validator_callback, auto_ack=True)
+    channel.basic_consume('to-validator', on_validator_callback)
     channel.start_consuming()
-
 
 if __name__ == '__main__':
     main()
