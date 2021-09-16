097f61ecbe5c3c1e67c8d7860c84dbdf66164d80
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-09-16 12:18:51 -0500

    refactored main callback into load_message and publish_message to help test

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import re
 import kubernetes
 
 
-WAGGLE_NODE_ID = os.environ['WAGGLE_NODE_ID'].lower()
-WAGGLE_NODE_VSN = os.environ.get('WAGGLE_NODE_VSN', '').upper()
-RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST', 'rabbitmq-server')
-RABBITMQ_PORT = int(os.environ.get('RABBITMQ_PORT', '5672'))
-RABBITMQ_USERNAME = os.environ.get('RABBITMQ_USERNAME', 'service')
-RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_PASSWORD', 'service')
+WAGGLE_NODE_ID = os.environ.get("WAGGLE_NODE_ID", "")
+WAGGLE_NODE_VSN = os.environ.get("WAGGLE_NODE_VSN", "")
+RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST", "rabbitmq-server")
+RABBITMQ_PORT = int(os.environ.get("RABBITMQ_PORT", "5672"))
+RABBITMQ_USERNAME = os.environ.get("RABBITMQ_USERNAME", "service")
+RABBITMQ_PASSWORD = os.environ.get("RABBITMQ_PASSWORD", "service")
 
 
 user_id_pattern_v1 = re.compile(r"^plugin\.(\S+:\d+\.\d+\.\d+)$")
@@ -41,98 +41,99 @@ def match_plugin_user_id(s):
     return None
 
 
-def update_pod_node_names(pod_node_name):
+def update_pod_node_names(pods):
     # clear all existing items since we will read in everything that's actually Running
-    pod_node_name.clear()
+    pods.clear()
     # scan pods from kubernetes
     v1api = kubernetes.client.CoreV1Api()
     ret = v1api.list_namespaced_pod("default")
-    for i in ret.items:
-        pod_node_name[i.metadata.uid] = i.spec.node_name
+    for item in ret.items:
+        pods[item.metadata.uid] = item
+
+
+class InvalidMessageError(Exception):
+
+    def __init__(self, error):
+        self.error = error
+
+
+def load_message(pods, properties, body):
+    try:
+        msg = message.load(body)
+    except json.JSONDecodeError:
+        raise InvalidMessageError(f"failed to parse message body {body}")
+    except KeyError as key:
+        raise InvalidMessageError(f"message missing key {key}")
+
+    # add metadata derived from pod info
+    if properties.app_id is not None:
+        pod_uid = properties.app_id
+        try:
+            pod = pods[pod_uid]
+        except KeyError:
+            raise InvalidMessageError(f"unable to find pod node name for {pod_uid}")
+        msg.meta["host"] = pod.spec.node_name
+
+    # add plugin metadata
+    plugin = match_plugin_user_id(properties.user_id)
+    if plugin is None:
+        raise InvalidMessageError(f"invalid message user_id {properties.user_id!r}")
+    msg.meta["plugin"] = plugin
+
+    # add node metadata
+    if WAGGLE_NODE_ID != "":
+        msg.meta["node"] = WAGGLE_NODE_ID
+    if WAGGLE_NODE_VSN != "":
+        msg.meta["vsn"] = WAGGLE_NODE_VSN
+
+    return msg
+
+
+def publish_message(ch, scope, msg):
+    body = message.dump(msg)
+
+    if scope not in {"node", "beehive", "all"}:
+        raise InvalidMessageError(f"invalid message scope {scope!r}")
+
+    if scope in {"node", "all"}:
+        logging.debug("forwarding message type %r to local", msg.name)
+        ch.basic_publish(
+            exchange="data.topic",
+            routing_key=msg.name,
+            body=body)
+
+    if scope in {"beehive", "all"}:
+        logging.debug("forwarding message type %r to beehive", msg.name)
+        ch.basic_publish(
+            exchange="to-beehive",
+            routing_key=msg.name,
+            body=body)
 
 
 def create_on_validator_callback():
-    pod_node_name = {}
+    pods = {}
 
     def on_validator_callback(ch, method, properties, body):
-        logging.debug("processing message")
+        logging.debug("processing message.")
+
+        # update cached pod info when we receive an unknown pod UID
+        # TODO think about rogue case where a made up UID is rapidly sent
+        if properties.app_id is not None and properties.app_id not in pods:
+            logging.info("got new pod uid %s. updating pod metadata...", properties.app_id)
+            update_pod_node_names(pods)
+            logging.info("updated pod metadata.")
 
         try:
-            msg = message.load(body)
-        except json.JSONDecodeError:
-            logging.warning('failed to parse message %s', body)
+            msg = load_message(pods, properties, body)
+            publish_message(ch, method.routing_key, msg)
+        except InvalidMessageError:
+            logging.exception("dropping invalid message.")
             ch.basic_ack(method.delivery_tag)
             return
-        except KeyError as key:
-            logging.warning('message missing key %s', key)
-            ch.basic_ack(method.delivery_tag)
-            return
-
-        # update the cached pod metadata if we encounter a new app_id
-        # NOTE this is expected to happen infrequently. if it is happen frequently, it indicates
-        # the bigger problem that plugin scheduling is thrashing.
-        if properties.app_id is not None:
-            pod_uid = properties.app_id
-
-            # update pod metadata, if needed
-            if pod_uid not in pod_node_name:
-                logging.info('got new pod uid %s. updating pod metadata...', pod_uid)
-                update_pod_node_names(pod_node_name)
-                logging.info('updated pod metadata.')
-
-            # add host metadata
-            try:
-                msg.meta["host"] = pod_node_name[pod_uid]
-            except KeyError:
-                logging.exception("unable to find pod node name for %s", pod_uid)
-        else:
-            logging.warning('message missing amqp app_id property %r', msg)
-
-        # TODO(sean) now we an totally clean up the dependence on user_id here...
-        # all the actual metadata will be discoverable via a look up k3s
-        # for example, we can just get all the annotations / labels from the metadata fields
-        # waggle.io
-
-        # validate and add plugin metadata
-        plugin = match_plugin_user_id(properties.user_id)
-        if plugin is None:
-            logging.warning('invalid message user_id %r', properties.user_id)
-            ch.basic_ack(method.delivery_tag)
-            return
-        msg.meta["plugin"] = plugin
-
-        # add node metadata
-        msg.meta["node"] = WAGGLE_NODE_ID
-
-        # add vsn metadata, if nonempty
-        if WAGGLE_NODE_VSN != '':
-            msg.meta["vsn"] = WAGGLE_NODE_VSN
-
-        body = message.dump(msg)
-        scope = method.routing_key
-
-        if scope not in ['node', 'beehive', 'all']:
-            logging.warning('invalid message scope %s', scope)
-            ch.basic_ack(method.delivery_tag)
-            return
-
-        if scope in ['node', 'all']:
-            logging.debug('forwarding message type "%s" to local', msg.name)
-            ch.basic_publish(
-                exchange='data.topic',
-                routing_key=msg.name,
-                body=body)
-
-        if scope in ['beehive', 'all']:
-            logging.debug('forwarding message type "%s" to beehive', msg.name)
-            ch.basic_publish(
-                exchange='to-beehive',
-                routing_key=msg.name,
-                body=body)
 
         ch.basic_ack(method.delivery_tag)
-        logging.debug('processed message')
-    
+        logging.debug("processed message.")
+
     return on_validator_callback
 
 
@@ -144,20 +145,20 @@ def declare_exchange_with_queue(ch: pika.adapters.blocking_connection.BlockingCh
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--debug', action='store_true', help='enable verbose logging')
-    parser.add_argument('--kubeconfig', default=None, help='kubernetes config')
-    parser.add_argument('--rabbitmq-host', default=RABBITMQ_HOST, help='rabbitmq host')
-    parser.add_argument('--rabbitmq-port', default=RABBITMQ_PORT, type=int, help='rabbitmq port')
-    parser.add_argument('--rabbitmq-username', default=RABBITMQ_USERNAME, help='rabbitmq username')
-    parser.add_argument('--rabbitmq-password', default=RABBITMQ_PASSWORD, help='rabbitmq password')
+    parser.add_argument("--debug", action="store_true", help="enable verbose logging")
+    parser.add_argument("--kubeconfig", default=None, help="kubernetes config")
+    parser.add_argument("--rabbitmq-host", default=RABBITMQ_HOST, help="rabbitmq host")
+    parser.add_argument("--rabbitmq-port", default=RABBITMQ_PORT, type=int, help="rabbitmq port")
+    parser.add_argument("--rabbitmq-username", default=RABBITMQ_USERNAME, help="rabbitmq username")
+    parser.add_argument("--rabbitmq-password", default=RABBITMQ_PASSWORD, help="rabbitmq password")
     args = parser.parse_args()
 
     logging.basicConfig(
         level=logging.DEBUG if args.debug else logging.INFO,
-        format='%(asctime)s %(message)s',
-        datefmt='%Y/%m/%d %H:%M:%S')
+        format="%(asctime)s %(message)s",
+        datefmt="%Y/%m/%d %H:%M:%S")
     # pika logging is too verbose, so we turn it down.
-    logging.getLogger('pika').setLevel(logging.CRITICAL)
+    logging.getLogger("pika").setLevel(logging.CRITICAL)
 
     # load incluster service account config
     # NOTE the service account needs read access to pod info
@@ -173,23 +174,23 @@ def main():
             username=args.rabbitmq_username,
             password=args.rabbitmq_password,
         ),
-        client_properties={'name': 'data-sharing-service'},
+        client_properties={"name": "data-sharing-service"},
         connection_attempts=3,
         retry_delay=10,
     )
 
-    logging.info('connecting to rabbitmq server at %s:%d as %s.', params.host, params.port, params.credentials.username)
+    logging.info("connecting to rabbitmq server at %s:%d as %s.", params.host, params.port, params.credentials.username)
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
 
-    logging.info('setting up queues and exchanges.')
-    channel.exchange_declare('data.topic', exchange_type='topic', durable=True)
-    declare_exchange_with_queue(channel, 'to-validator')
-    declare_exchange_with_queue(channel, 'to-beehive')
+    logging.info("setting up queues and exchanges.")
+    channel.exchange_declare("data.topic", exchange_type="topic", durable=True)
+    declare_exchange_with_queue(channel, "to-validator")
+    declare_exchange_with_queue(channel, "to-beehive")
     
-    logging.info('starting main process.')
-    channel.basic_consume('to-validator', create_on_validator_callback())
+    logging.info("starting main process.")
+    channel.basic_consume("to-validator", create_on_validator_callback())
     channel.start_consuming()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,5 +1,26 @@
 import unittest
-from main import match_plugin_user_id
+import pika
+from waggle import message
+from kubernetes.client import V1Pod, V1PodSpec, V1ObjectMeta, V1ContainerState
+from main import match_plugin_user_id, load_message
+
+
+test_pods_list = [
+    V1Pod(
+        api_version="v1",
+        kind="Pod",
+        metadata=V1ObjectMeta(
+            uid="9a28e690-ad5d-4027-90b3-1da2b41cf4d1",
+            labels={"role": "plugin"}
+        ),
+        spec=V1PodSpec(
+            node_name="1.rpi",
+            containers=[],
+        ),
+    )
+]
+
+test_pods = {pod.metadata.uid: pod for pod in test_pods_list}
 
 
 class TestMain(unittest.TestCase):
@@ -13,6 +34,23 @@ class TestMain(unittest.TestCase):
 
         for user_id, want in tests:
             self.assertEqual(match_plugin_user_id(user_id), want)
+    
+    def test_load_message(self):
+        properties = pika.BasicProperties(
+            app_id="9a28e690-ad5d-4027-90b3-1da2b41cf4d1",
+            user_id="plugin.plugin-metsense:1.2.3",
+        )
+        body = message.dump(message.Message(
+            timestamp=1360287003083988472,
+            name="test",
+            value=23.1,
+            meta={
+                "camera": "left",
+            }
+        ))
+
+        msg = load_message(test_pods.copy(), properties, body)
+        self.assertEqual(msg.meta["host"], "1.rpi")
 
 
 if __name__ == "__main__":
