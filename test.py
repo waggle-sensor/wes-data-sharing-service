a1e3984d8c2b143c1ab4fecfad281c271f9f1adc
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-03-22 22:59:58 -0500

    cleaned up tests

--- test.py
+++ test.py
@@ -6,35 +6,33 @@ from pod_event_watcher import Pod
 import wagglemsg
 
 
+def make_test_handler():
+    return MessageHandler(
+        config=MessageHandlerConfig(
+            node="0000000000000001",
+            vsn="W001",
+            upload_publish_name="upload",
+        ),
+        publisher=Publisher(channel=None),
+    )
+
+
 class TestMessageHandler(unittest.TestCase):
 
     def test_missing_uid(self):
-        handler = MessageHandler(
-            config=MessageHandlerConfig(
-                node="0000000000000001",
-                vsn="W001",
-                upload_publish_name="upload",
-            ),
-            publisher=None,
-        )
+        handler = make_test_handler()
         delivery = Delivery(pod_uid=None)
+
+        handler.publisher.publish = MagicMock()
         delivery.ack = MagicMock()
+        
         handler.handle_delivery(delivery)
 
+        handler.publisher.publish.assert_not_called()
         delivery.ack.assert_called_once()
     
     def test_expect_task_label(self):
-        publisher = Publisher(channel=None)
-        publisher.publish = MagicMock()
-
-        handler = MessageHandler(
-            config=MessageHandlerConfig(
-                node="0000000000000001",
-                vsn="W001",
-                upload_publish_name="upload",
-            ),
-            publisher=publisher,
-        )
+        handler = make_test_handler()
 
         pod = Pod(
             uid="some-uid",
@@ -55,28 +53,18 @@ class TestMessageHandler(unittest.TestCase):
                 meta={},
             )),
         )
+
+        handler.publisher.publish = MagicMock()
         delivery.ack = MagicMock()
 
         handler.handle_pod(pod)
         handler.handle_delivery(delivery)
 
+        handler.publisher.publish.assert_not_called()
         delivery.ack.assert_called_once()
-        publisher.publish.assert_not_called()
 
     def test_handle_pod_then_delivery(self):
-        config = MessageHandlerConfig(
-            node="0000000000000001",
-            vsn="W001",
-            upload_publish_name="upload",
-        )
-
-        publisher = Publisher(channel=None)
-        publisher.publish = MagicMock()
-
-        handler = MessageHandler(
-            config=config,
-            publisher=publisher,
-        )
+        handler = make_test_handler()
 
         pod = Pod(
             uid="some-uid",
@@ -103,14 +91,14 @@ class TestMessageHandler(unittest.TestCase):
             pod_uid="some-uid",
             body=body,
         )
+
+        handler.publisher.publish = MagicMock()
         delivery.ack = MagicMock()
 
         handler.handle_pod(pod)
         handler.handle_delivery(delivery)
 
-        delivery.ack.assert_called_once()
-
-        self.assert_published(publisher, ["data.topic", "to-beehive"], wagglemsg.Message(
+        self.assert_published(handler.publisher, ["data.topic", "to-beehive"], wagglemsg.Message(
             name=msg.name,
             value=msg.value,
             timestamp=msg.timestamp,
@@ -119,25 +107,14 @@ class TestMessageHandler(unittest.TestCase):
                 "job": "sage",
                 "task": "example",
                 "plugin": "plugin-example:1.2.3",
-                "node": config.node,
-                "vsn": config.vsn,
+                "node": handler.config.node,
+                "vsn": handler.config.vsn,
             },
         ))
+        delivery.ack.assert_called_once()
 
     def test_handle_delivery_then_pod(self):
-        config = MessageHandlerConfig(
-            node="0000000000000001",
-            vsn="W001",
-            upload_publish_name="upload",
-        )
-
-        publisher = Publisher(channel=None)
-        publisher.publish = MagicMock()
-
-        handler = MessageHandler(
-            config=config,
-            publisher=publisher,
-        )
+        handler = make_test_handler()
 
         pod = Pod(
             uid="some-uid",
@@ -164,14 +141,14 @@ class TestMessageHandler(unittest.TestCase):
             pod_uid="some-uid",
             body=body,
         )
+
+        handler.publisher.publish = MagicMock()
         delivery.ack = MagicMock()
 
         handler.handle_delivery(delivery)
         handler.handle_pod(pod)
 
-        delivery.ack.assert_called_once()
-
-        self.assert_published(publisher, ["data.topic", "to-beehive"], wagglemsg.Message(
+        self.assert_published(handler.publisher, ["data.topic", "to-beehive"], wagglemsg.Message(
             name=msg.name,
             value=msg.value,
             timestamp=msg.timestamp,
@@ -180,10 +157,11 @@ class TestMessageHandler(unittest.TestCase):
                 "job": "sage",
                 "task": "example",
                 "plugin": "plugin-example:1.2.3",
-                "node": config.node,
-                "vsn": config.vsn,
+                "node": handler.config.node,
+                "vsn": handler.config.vsn,
             },
         ))
+        delivery.ack.assert_called_once()
 
     def assert_published(self, publisher, exchanges, msg):
         calls = publisher.publish.call_args_list
